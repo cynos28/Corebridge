7a74a6e3ad4be8626462dc72dc5bfa8929e4da36
dhanuka prabhashwara <dhanukaprabha@gmail.com>
2025-03-10 22:47:36 +0530

    added some styles

---
+++ client/src/app/Dhanuka/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./App.css"; // Import the CSS file
+
+const App: React.FC = () => {
+  return <h1 className="hello-world">Hello, World! 👋</h1>;
+};
+
+export default App;
