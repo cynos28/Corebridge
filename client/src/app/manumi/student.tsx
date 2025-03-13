e37a143fe0b84a20bc0a1478a7294dfd372fe3ff
ManumiThilothma <manumithilothma618@gmail.com>
2025-03-13 09:33:17 +0530

    page creation

---
+++ client/src/app/manumi/student.tsx
@@ -0,0 +1,13 @@
+type StudentProps = {
+    name: string;
+    grade: string;
+  };
+  
+  const Student = ({ name, grade }: StudentProps) => {
+    return 
+    
+    
+  };
+  
+  export default Student;
+  
