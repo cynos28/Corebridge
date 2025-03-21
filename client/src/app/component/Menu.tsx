18a5501426f3d7c9892a971d7cb20e80bdf83dfb
cynos28 <118928202+cynos28@users.noreply.github.com>
2025-03-22 01:57:32 +0530

    menu tsx role

--- client/src/app/component/Menu.tsx
+++ client/src/app/component/Menu.tsx
@@ -125,19 +125,24 @@ const menuItems = [
       <div className="mt-4 text-sm">
         {menuItems.map(i => (
           <div className="flex flex-col gap-2" key={i.title}>
-            <span className="hidden lg:block text-gray-400 font-light my-4">{i.title}</span>
-            {i.items.map(item => (
-              <Link 
-              href={item.href} 
-              key={item.label}
-              className="flex item-center justify-center lg:justify-start gap-4 text-gray-500 py-2">
-                
-                  <img src={item.icon} alt="" width={20} height={20} />
+          <span className="hidden lg:block text-gray-400 font-light my-4">
+            {i.title}
+          </span>
+          {i.items.map((item) => {
+            if (item.visible.includes(role)) {
+              return (
+                <Link
+                  href={item.href}
+                  key={item.label}
+                  className="flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2 md:px-2 rounded-md hover:bg-cbSkyLight"
+                >
+                  <Image src={item.icon} alt="" width={20} height={20} />
                   <span className="hidden lg:block">{item.label}</span>
-               
-              </Link>
-            ))}
-          </div>
+                </Link>
+              );
+            }
+          })}
+        </div>
         ))}
       </div>
     );
