5a0e8751252690a654f796fd8b867631dc45d298
cynos28 <118928202+cynos28@users.noreply.github.com>
2025-03-22 02:35:56 +0530

    nav bar redesign

--- client/src/app/dashboard/student/page.tsx
+++ client/src/app/dashboard/student/page.tsx
@@ -1,7 +1,25 @@
-const StudentPage = () => {
+import Announcement from "@/app/component/Announcement";
+import BigCalendar from "@/app/component/BigCalendar";
+import EventCalendar from "@/app/component/EventCalendar";
+
+
+const Student = () => {
   return (
-    <div className=''>StudentPage</div>
+    <div className="p-4 flex gap-4 flex-col xl:flex-row">
+      {/* LEFT */}
+      <div className="w-full xl:w-2/3">
+        <div className="h-full bg-white p-4 rounded-md">
+          <h1 className="text-xl font-semibold">Schedule (4A)</h1>
+          <BigCalendar/>
+        </div>
+      </div>
+      {/* RIGHT */}
+      <div className="w-full xl:w-1/3 flex flex-col gap-8">
+        <EventCalendar />
+        <Announcement />
+      </div>
+    </div>
   );
 };
 
-export default StudentPage;
+export default Student;
