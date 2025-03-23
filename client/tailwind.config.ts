60ca1e675a6678b375ea01b41913d225305e63d5
cynos28 <118928202+cynos28@users.noreply.github.com>
2025-03-23 20:30:18 +0530

    color updates

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -22,7 +22,7 @@ const config: Config = {
   			'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
   		},
   		colors: {
-  			cbSky: '#C3EBFA',
+  			cbSky: '#CDE8E5',
   			cbSkyLight: '#EDF9FD',
   			cbPurple: '#CDE8E5',
   			cbPurpleLight: '#D6C0B3',
